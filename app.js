56c848401db40cc1c90626c880e9688794612d2b
vinayakokc9 <vinayakokc9@gmail.com>
2023-11-10 00:07:28 -0600

    Add shopping cart feature and redesigned search results

--- app.js
+++ app.js
@@ -30,5 +30,31 @@ function addtoCart(event) {
     var productPicture = document.querySelector(".product-picture").src
     console.log(productPicture)    
 
+    addItem(productName, productPrice, productPicture)
+
+}
+
+function addItem(name, price, picture) {
+    var cartWindow = window.open('cart.html')
+
+    cartWindow.onload = function() {
+        var cartRow = cartWindow.document.createElement('div')
+        var cartItems = cartWindow.document.getElementsByClassName('product-list')[0]
+        var itemContent = `
+        <div class="row border-top border-bottom">
+          <div class="row align-items-center product-item">
+            <div class="col-2"><img src="${picture}" alt="" class="img-fluid"></div>
+            <div class="col">
+              <div class="row cart-title">${name}</div>
+            </div>
+            <div class="col cart-quantity">Quantity: 1</div>
+            <div class="col cart-price">${price}</div>
+          </div>
+        </div>
+        `
+        cartRow.innerHTML = itemContent
+        cartItems.append(cartRow)
+    }
+    
 
 }
