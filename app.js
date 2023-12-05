a7a57b80157d4cb679579e5e36de3b5c3cc4b16b
vinayakokc9 <34376462+vinayakokc9@users.noreply.github.com>
2023-12-05 13:54:01 -0600

    Revert "Update comparison"

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ if (document.readyState == 'loading') {
 } else {
     ready()
 }
-
 function ready() {
     var addCartButton = document.getElementsByClassName('btn-warning')
     for (var i = 0; i < addCartButton.length; i++) {
@@ -12,48 +11,25 @@ function ready() {
     }
 
     var userInput = document.getElementsByClassName('search-box')[0]
-    // console.log(userInput)
+    console.log(userInput)
     userInput.addEventListener('keypress', function (event) {
-        if (event.key === 'Enter') {
+        if(event.key === 'Enter') {
             event.preventDefault()
             document.getElementsByClassName("btn-outline-secondary")[0].click()
         }
     })
-
+    
 }
 
 function searchBehavior() {
     var searchInput = document.getElementsByClassName('search-box')[0].value
-    var playstationItem = document.getElementsByClassName('playstation-item')[0];
-    var xboxItem = document.getElementsByClassName('xbox-item')[0];
-    var switchItem = document.getElementsByClassName('switch-item')[0];
-  
-    if(!xboxItem) {
-        console.error("Not found")
-    }
-    const searchKeywords = [ "xbox", "switch", "nintendo switch", "gaming consoles", "playstation", "play station", "video games", "games"]
-    for(var i = 0 ; i < searchKeywords.length; i++) {
-        if (searchKeywords[i].includes(searchInput.toLowerCase()) && searchInput.length > 3) {
-            
-            if(i < 1) {
-               window.location.href = "searchResultsXbox.html" 
-            }
-            else if(i < 3) {
-                window.location.href = "searchResultsSwitch.html" 
-            }
-            else {
-                window.location.href = "searchResults.html" 
-            }
-            
-            
+    console.log(searchInput)
+    const searchKeywords = ["playstation", "play station", "xbox", "switch", "nintendo switch", "video games", "games"]
 
-            return;
-        }
-        
+    if(searchKeywords.includes(searchInput.toLowerCase())) {
+        window.location.href = "searchResults.html"   
     }
-    
-     
-    if (searchInput == "") {
+    else if (searchInput == ""){
         alert("Search box cannot be blank. Please enter a value in the search field.")
     }
     else {
@@ -61,21 +37,17 @@ function searchBehavior() {
     }
 }
 
-
-
-
-
-function updateSummary(cartWindow, cartLength, price) {
-    // updating total item count
+function  updateSummary(cartWindow, cartLength, price) {
+// updating total item count
     var itemCount = cartWindow.document.getElementById('total-items')
     itemCount.innerText = cartLength
-
+    
     // updating total price
     var totalPrice = cartWindow.document.getElementById('total-price');
     totalPrice.innerText = price
 
 
-
+    
 }
 function addtoCart(event) {
     var button = event.target
@@ -88,7 +60,7 @@ function addtoCart(event) {
     console.log(productPrice)
 
     var productPicture = document.querySelector(".product-picture").src
-    console.log(productPicture)
+    console.log(productPicture)    
 
     addItem(productName, productPrice, productPicture)
 
@@ -96,17 +68,15 @@ function addtoCart(event) {
 
 function addItem(name, price, picture) {
     var cartWindow = window.open('cart.html')
+    
 
-
-    cartWindow.onload = function () {
+    cartWindow.onload = function() {
         var cartRow = cartWindow.document.createElement('div')
         var cartItems = cartWindow.document.getElementsByClassName('product-list')[0]
-        var itemContent =
-            `
+        var itemContent = `
         <div class="row border-top border-bottom">
           <div class="row align-items-center product-item">
-            <div class="col-2 ps-0">
-                <img src="${picture}" alt="" class="img-fluid"></div>
+            <div class="col-2 ps-0"><img src="${picture}" alt="" class="img-fluid"></div>
             <div class="col">
               <div class="row cart-title">${name}</div>
             </div>
@@ -120,14 +90,12 @@ function addItem(name, price, picture) {
 
         // update number of items in cart
         var cartCount = cartWindow.document.getElementsByClassName('product-item').length
-
+       
         // update total price
         var totalPrice = 0;
         var priceList = cartWindow.document.querySelectorAll('.cart-price')
         console.log(priceList)
 
-
-        
         priceList.forEach(function (item) {
             var priceText = item.innerText.replace('Price: $', '')
             var price = parseFloat(priceText);
@@ -140,113 +108,6 @@ function addItem(name, price, picture) {
         updateSummary(cartWindow, cartCount, totalPrice);
 
     }
+    
 
-
-}
-
-var gameConsoles = {
-    "Playstation": {
-        "Type": "Console",
-        "Color": "White",
-        "Weight": "14.7 lbs",
-        "Customer Reviews": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
-    },
-    "Xbox": {
-        "Type": "Console",
-        "Color": "Black",
-        "Weight": "9.7 lbs",
-        "Customer Reviews": "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
-    },
-    "Switch": {
-        "Type": "Handheld",
-        "Color": "Black",
-        "Weight": "0.66 lbs",
-        "Customer Reviews": "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?"
-    }
-};
-
-function updateTable() {
-    var selectedFeatures = getSelectedValues("featuresDropdownContent");
-    var selectedProducts = getSelectedValues("productsDropdownContent");
-    var tableBody = document.getElementById("tableBody");
-    var tableCol = document.getElementById("tableCol");
-    var comparisonTable = document.getElementById("comparisonTable");
-    var selectedFeaturesSpan = document.getElementById("selectedFeatures");
-    var selectedProductsSpan = document.getElementById("selectedProducts");
-
-
-    tableBody.innerHTML = "";
-
-    // update selected product list 
-    if (selectedProducts.length > 0) {
-        selectedProductsSpan.textContent = selectedProducts.join(', ');
-    }
-    else {
-        selectedProductsSpan.textContent = "None";
-    }
-
-    // update selected features list
-    if (selectedFeatures.length > 0) {
-        selectedFeaturesSpan.textContent = selectedFeatures.join(', ');
-    }
-    else {
-        selectedFeaturesSpan.textContent = "None";
-    }
-
-
-    //reset header cells
-    if (tableCol.childElementCount > 0) {
-        for (var i = 1; i < tableCol.childElementCount; i++) {
-            tableCol.deleteCell(i);
-            i--;
-        }
-    }
-
-    //insert product columns
-    for (var i = 0; i < selectedProducts.length; i++) {
-        var featureCell = tableCol.insertCell(i + 1);
-        featureCell.innerText = selectedProducts[i];
-
-    }
-
-    //populate table body
-    if (selectedProducts.length > 0 && selectedFeatures.length > 0) {
-
-        comparisonTable.style.display = "table";
-
-        // create list of products and items
-
-        selectedProductsSpan.textContent = selectedProducts.join(', ');
-        selectedFeaturesSpan.textContent = selectedFeatures.join(', ');
-
-        // populate rows
-        for (var i = 0; i < selectedFeatures.length; i++) {
-            var newRow = tableBody.insertRow(tableBody.rows.length);
-            var featureCell = newRow.insertCell(0);
-            featureCell.innerText = selectedFeatures[i];
-
-            for (var j = 0; j < selectedProducts.length; j++) {
-                var productCell = newRow.insertCell(j + 1);
-                productCell.innerText = getData(selectedProducts[j], selectedFeatures[i]);
-            }
-        }
-    } else {
-        comparisonTable.style.display = "none";
-    }
-}
-
-function getSelectedValues(dropdownId) {
-    var dropdownContent = document.getElementById(dropdownId);
-    var selectedValues = [];
-    var activeCheckboxes = dropdownContent.querySelectorAll('input[type="checkbox"]:checked');
-
-    for (var i = 0; i < activeCheckboxes.length; i++) {
-        selectedValues.push(activeCheckboxes[i].value);
-    }
-    return selectedValues;
-}
-
-function getData(product, feature) {
-
-    return gameConsoles[product][feature];
 }
